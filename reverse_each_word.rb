eef7ed88d22313d96248b5088b8d4de93b1d7f82
Ting Chung <tchung682@gmail.com>
2020-06-08 12:17:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,12 @@
 def reverse_each_word(text)
     newText = text.split(" ")
     reverse_word = ""
-    newText.collect do |word|
+    
+    collect_return = newText.collect do |word|
         reverse_word = reverse_word + word.reverse + " "
         
     end
-    newText = reverse_word.split(" ")
-    newText.join(" ")
+    #newText = reverse_word.split(" ")
+    #newText.join(" ")
     
 end
